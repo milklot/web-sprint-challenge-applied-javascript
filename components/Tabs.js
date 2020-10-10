e7a2d1460825c71242d6e16fd1a767b1788ef75e
milklot <vbiostflex@gmail.com>
2020-10-10 17:12:04 -0400

    done with tabs

--- components/Tabs.js
+++ components/Tabs.js
@@ -1,3 +1,5 @@
+import Axios from "axios";
+
 // STEP 2: Create tabs
 // -----------------------
 // Using axios send a GET request to the address: https://lambda-times-api.herokuapp.com/topics
@@ -9,3 +11,25 @@
 //    <div class="tab">topic here</div>
 //
 // NOTE: you do _not_ need to install axios as it's included in the HTML via script element
+
+Axios.get('https://lambda-times-api.herokuapp.com/topics')
+    .then(result => console.log(result))
+
+const tabMaker = (tabName) => {
+    const result = document.createElement('div');
+
+    result.classList.add('tab');
+    result.textContent = tabName;
+    return result;
+}
+
+Axios.get('https://lambda-times-api.herokuapp.com/topics')
+    .then(result => {
+        const topics = result.data.topics;
+
+        topics.forEach(data => {
+            const finalTab = tabMaker(data);
+            document.querySelector('.topics').appendChild(finalTab);
+        })
+    })
+    .catch(fail => console.log('something went wrong: ', fail));
